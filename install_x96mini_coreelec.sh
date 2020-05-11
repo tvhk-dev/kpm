f4d3dea57d49ef53c3a9e6c6343d0ec4af40294b
zenzorship <oldhoteltv@gmail.com>
2020-05-12 02:40:15 +0800

    install script mirror fix add remote config

--- install_x96mini_coreelec.sh
+++ install_x96mini_coreelec.sh
@@ -12,15 +12,16 @@ extraRepo="https://raw.githubusercontent.com/tvhk-dev/tvhk-kodi-repo/master/kpmR
 #
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-read -p "Did you finish running installtointernal? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    echo "OK"
-else
-    echo "Go for installtointernal first!"
-    exit 1
-fi
+read -r -p "Did you finish running installtointernal? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY])
+        echo "OK"
+        ;;
+    *)
+        echo "Go for installtointernal first!"
+        exit 1
+        ;;
+esac
 
 printf 'n\n' | installentware #https://discourse.coreelec.org/t/what-is-entware-and-how-to-install-uninstall-it/1149
 
@@ -34,7 +35,7 @@ cd ~/install
 cd ~/install/kpm
 ~/.opt/bin/npm install
 mkdir ~/install/addons
-~/.opt/bin/node ./kpm.js --to=/storage/install/addons --kodi=$kodiVersion --extra=$extraRepo $addons -y
+~/.opt/bin/node ./kpm.js --to=/storage/install/addons --kodi=$kodiVersion --overlay=$extraRepo $addons -y
 cd ~/install/addons
 cp -Rf ./* ~/.kodi/addons/
 systemctl restart kodi
@@ -45,4 +46,12 @@ for dir in */; do sqlite3 /storage/.kodi/userdata/Database/Addons27.db "update i
 #swtich skin
 sed -i -e 's/skin\.estuary/skin\.aura-tvhk/g' /storage/.kodi/userdata/guisettings.xml
 sed -i -e 's/default="true">skin\.aura-tvhk/>skin\.aura-tvhk/g' /storage/.kodi/userdata/guisettings.xml
+
+#remote
+systemctl stop eventlircd
+echo "meson-ir        *     /storage/.config/rc_keymaps/X96" >> /storage/.config/rc_maps.cfg
+printf "0x140 KEY_POWER\n0x144 KEY_SUBTITLE\n0x155 KEY_PREVIOUS\n0x15a KEY_PLAYPAUSE\n0x152 KEY_STOP\n0x154 KEY_NEXT\n0x143 KEY_CONFIG\n0x10f KEY_INFO\n0x110 KEY_VOLUMEDOWN\n0x118 KEY_VOLUMEUP\n0x111 KEY_HOME\n0x119 KEY_BACK\n0x14c KEY_MENU\n0x100 KEY_CONTEXT_MENU\n0x116 KEY_UP\n0x151 KEY_LEFT\n0x150 KEY_RIGHT\n0x11a KEY_DOWN\n0x113 KEY_OK\n0x101 KEY_NUMERIC_0\n0x14e KEY_NUMERIC_1\n0x10d KEY_NUMERIC_2\n0x10c KEY_NUMERIC_3\n0x14a KEY_NUMERIC_4\n0x109 KEY_NUMERIC_5\n0x108 KEY_NUMERIC_6\n0x146 KEY_NUMERIC_7\n0x105 KEY_NUMERIC_8\n0x104 KEY_NUMERIC_9\n0x141 KEY_MUTE\n0x142 KEY_BACKSPACE\n" > /storage/.config/rc_keymaps/X96
+ir-keytable -a /storage/.config/rc_maps.cfg -s rc0
+systemctl start eventlircd
+
 systemctl start kodi
